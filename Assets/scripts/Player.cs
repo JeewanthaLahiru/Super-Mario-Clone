24225233a9b36d024b05d1a799e874d8b7dfc1c1
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-29 14:17:32 +0530

    created two scripts for player and camera

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Vector2 velocity;
+
+    private bool walk, walk_left, walk_right, jump;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,45 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        checkPlayerInput();
+        updatePlayerPosition();
+
+    }
+
+    //Update player with the input
+    void updatePlayerPosition()
+    {
+        Vector3 pos = transform.localPosition;
+        Vector3 scale = transform.localScale;
+
+        if (walk)
+        {
+            if (walk_left)
+            {
+                pos.x -= velocity.x * Time.deltaTime;
+                scale.x = -1;
+            }
+            if (walk_right)
+            {
+                pos.x += velocity.x * Time.deltaTime;
+                scale.x = 1;
+            }
+        }
+        transform.localPosition = pos;
+        transform.localScale = scale;
+    }
+    
+    //check player input method 
+    void checkPlayerInput()
+    {
+        bool input_left = Input.GetKey(KeyCode.LeftArrow);
+        bool input_right = Input.GetKey(KeyCode.RightArrow);
+        bool input_space = Input.GetKeyDown(KeyCode.Space);
+
+        walk = input_left || input_right;
+        walk_left = input_left && !input_right;
+        walk_right = !input_left && input_right;
+
+        jump = input_space;
     }
 }
