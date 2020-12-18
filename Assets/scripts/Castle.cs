5c876eb38cf5aac5401b295e17be7e54ddf5dd9e
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-18 09:35:46 +0530

    castle and winning

--- Assets/scripts/Castle.cs
+++ Assets/scripts/Castle.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Castle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Player")
+        {
+            Application.LoadLevel("Win");
+        }
     }
 }
